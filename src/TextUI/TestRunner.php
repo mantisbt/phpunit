6b5e1ab15f80d1f7c16773b5c66af2fe1668f02a
Sebastian Bergmann <sb@sebastian-bergmann.de>
2022-01-30 14:07:21 +0100

    Make method names consistent

--- src/TextUI/TestRunner.php
+++ src/TextUI/TestRunner.php
@@ -190,7 +190,7 @@ final class TestRunner
 
         Event\Facade::seal();
 
-        $this->printer->write(
+        $this->printer->print(
             Version::getVersionString() . "\n"
         );
 
@@ -337,7 +337,7 @@ final class TestRunner
             }
         }
 
-        $this->printer->write("\n");
+        $this->printer->print("\n");
 
         if ($this->configuration->enforceTimeLimit() && !(new Invoker)->canInvokeWithTimeout()) {
             $this->writeMessage('Error', 'PHP extension pcntl is required for enforcing time limits');
@@ -493,7 +493,7 @@ final class TestRunner
                     $this->configuration->coverageTextShowOnlySummary()
                 );
 
-                $outputStream->write(
+                $outputStream->print(
                     $processor->process(CodeCoverage::instance(), $this->configuration->colors())
                 );
             }
@@ -524,7 +524,7 @@ final class TestRunner
         }
 
         if ($this->printer !== null) {
-            $this->printer->write($buffer);
+            $this->printer->print($buffer);
         } else {
             print $buffer;
         }
@@ -633,7 +633,7 @@ final class TestRunner
 
     private function codeCoverageGenerationStart(string $format): void
     {
-        $this->printer->write(
+        $this->printer->print(
             sprintf(
                 "\nGenerating code coverage report in %s format ... ",
                 $format
@@ -645,7 +645,7 @@ final class TestRunner
 
     private function codeCoverageGenerationSucceeded(): void
     {
-        $this->printer->write(
+        $this->printer->print(
             sprintf(
                 "done [%s]\n",
                 $this->timer()->stop()->asString()
@@ -655,7 +655,7 @@ final class TestRunner
 
     private function codeCoverageGenerationFailed(\Exception $e): void
     {
-        $this->printer->write(
+        $this->printer->print(
             sprintf(
                 "failed [%s]\n%s\n",
                 $this->timer()->stop()->asString(),
